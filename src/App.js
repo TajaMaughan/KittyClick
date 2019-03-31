1c995ab740154874058f89ce957aaae0bad347ca
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-03-30 18:48:15 -0600

    score update, checking array for duplicate clicks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // import CardContainer from "./components/CardContainer";
 import Tile from "./components/Tile";
 import cardList from "./cardList.json";
@@ -9,48 +9,78 @@ import Col from "react-bootstrap/Col";
 import "./style.css";
 // import Score from "./components/Score";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+class App extends Component {
+  state = {
+      cardList,
       score: 0,
-      id: 0,
-      isClicked: false
-    };
-  }
-
-  handleClick = event => {
-    let isClicked = this.state.clickedArray;
-    let score = this.state.score;
-    if (isClicked) {
-      this.setState({
-        isClicked: false,
-        score: 0
-      });
-    } else {
-      this.setState(
-        state => {
-          return {
-            isClicked: true,
-            id: 0,
-            score: ++score
-          };
-        },
-        () => {
-          if (this.state.score === 12) {
-            this.setState({
-              isClicked: false,
-              score: 0
-            });
-          }
-        }
-      );
+      clickedArray: [],
+      message: "Click an image to begin"
+      // isClicked: false
     }
-  };
+
+  handleClick = (event) => {
+    // Arrange the pictures in a random manner
+    // const shuffledArray = this.shuffleArray(cards);
+    // this.setState({cards: shuffledArray});
+    // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
+    if (this.state.clickedArray.includes(event.target.id)) {
+      this.setState({ 
+        score: 0, 
+        clickedArray: [],
+        message: "That's already been clicked. You lose!"
+      });
+    }
+    else {
+      this.setState({
+        clickedArray: this.state.clickedArray.concat(event.target.id),
+        score: this.state.score + 1,
+        message: "Good Guess! Keep going."
+        // shakeit: "false"
+      });
+    }
+    if(this.state.score === 12) {
+      this.setState({
+        score: 0,
+        clickedArray: [],
+        message: "You win!"
+      })
+    }
+  }
+  // handleClick = id => {
+  //   // let isClicked = this.state.isClicked;
+  //   // let id = event.target.id;
+  //   let score = this.state.score;
+  //   let clickedArray = this.state.clickedArray;
+  //   // const { target: { id, clicked } } = event;
+  //   if (clickedArray) {
+  //     this.setState({
+  //       // isClicked: false,
+  //       clickedArray: [],
+  //       score: 0
+  //     });
+  //   } else {
+      
+  //     this.setState({
+  //           clickedArray: this.state.clickedArray.concat([id]),
+  //           // isClicked[id]: true,
+  //           score: ++score
+  //         },
+
+  //       () => {
+  //         if (this.state.score === 12) {
+  //           this.setState({
+  //             isClicked: false,
+  //             score: 0
+  //           });
+  //         }
+  //       }
+  //     );
+  //   }
+  // };
   render() {
     return (
       <div>
-        <Header score={this.state.score} />
+        <Header score={this.state.score} message={this.state.message}/>
         <Container className="pt-2 pb-2">
           <Row className="margin">
             <Col>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,14 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Score from "../Score";
 
-const Header = ({ score }) => {
+const Header = ({ score, message }) => {
   return (
       <Container>
     <Jumbotron className="text-center pt-1 pb-1 mb-0">
       <h2>Test your Memory!</h2>
       <h6>Click on an image to start. Every time you click an image they will render in a different order. Make sure you don't click the same image twice or the score will reset. You win when your score equals 12</h6>
       <Score score={score} />
+      <h6>{message}</h6>
     </Jumbotron>
     </Container>
   );
